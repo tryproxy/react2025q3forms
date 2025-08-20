571eb70087008084a8accc7ee54dd43fed8e2d7b
asdqdsa <asdqdsa@protonmail.com>
2025-08-20 10:59:34 +0400

    feat: add modal

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,22 +1,34 @@
 import { cn } from '@/shared/lib/cn';
+import { UIButton } from '@/shared/uikit/ui-button';
+import { UIModal } from '@/shared/uikit/ui-modal';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div
+      data-theme="dark"
       className={cn(
-        'flex h-screen flex-col items-center justify-center bg-gray-500'
+        'bg-background text-surface-foreground flex h-screen flex-col items-center justify-center'
       )}
     >
-      <button
-        className="cursor-pointer font-bold text-black"
-        onClick={() => setCount(count + 1)}
-      >
-        [ CENTERED ]
-      </button>
-      <div>{count}</div>
+      <div id="modals"></div>
+      <UIModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <UIModal.Header>HEADER</UIModal.Header>
+        <UIModal.Body>TEXT</UIModal.Body>
+        <UIModal.Footer>
+          <UIButton variant="secondary">Submit</UIButton>
+        </UIModal.Footer>
+      </UIModal>
+      <div className="flex gap-4">
+        <UIButton variant="default" onClick={() => setIsOpen(true)}>
+          controlled
+        </UIButton>
+        <UIButton variant="secondary" onClick={() => setIsOpen(true)}>
+          uncontrolled
+        </UIButton>
+      </div>
     </div>
   );
 }
